421dbd1ccb0c6ffe360abe4a3a16a0814c39101d
Aden Addis <adenwonde56@gmail.com>
2022-07-02 01:38:51 -0400

    commit tree fba1857fa31db034b96230777cb708d0fa431caf

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+
+// assign and remove main
+const main = document.getElementById("main")
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "Aden is the champion"
+
+document.body.append(newHeader);
+
+
+// const newHeaderId = document.createElement("id")
+// newHeaderId.textContent = "victory"
+// newHeader.append(newHeaderId);
+
+
+
+
+// can use .textContent instead of .innerHTML
+
